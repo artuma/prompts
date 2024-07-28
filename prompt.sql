6e189e71388a6379682a773c9bcdaefa624e5585
artuma <165459314+artuma@users.noreply.github.com>
2024-07-28 09:33:35 +0500

    Update prompt.sql
    round too

--- prompt.sql
+++ prompt.sql
@@ -78,3 +78,10 @@ SELECT ROUND(100.5511)
 Результат:
 101.0
 
+SELECT product_id,
+       name,
+       price as old_price,
+       round(price*1.05, 1) as new_price
+FROM   products
+ORDER BY new_price desc, product_id
+-- с функцией round
