e06ea0ace5d0edfe1ee277bc0149d0b87f577b4a
artuma <165459314+artuma@users.noreply.github.com>
2024-08-03 10:10:38 +0500

    Update prompt.sql
    tax %

--- prompt.sql
+++ prompt.sql
@@ -108,3 +108,11 @@ SELECT product_id,
             else 0 end new_price
 FROM   products
 ORDER BY new_price desc, product_id
+
+SELECT product_id,
+       name,
+       price,
+       round(price/120*20, 2) as tax,
+       round(price - price/120*20, 2) as price_before_tax
+FROM   products
+ORDER BY price_before_tax desc, product_id
